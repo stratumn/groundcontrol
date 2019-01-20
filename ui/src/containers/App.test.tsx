977c6f854ba2950a33aed1e2bee59362c63181c9
Stephan Florquin <stephan.florquin@gmail.com>
2019-01-20 10:09:31 +0100

    ui: add first tests :)

--- ui/src/containers/App.test.tsx
+++ ui/src/containers/App.test.tsx
@@ -1,9 +1,19 @@
+import { shallow } from "enzyme";
 import React from "react";
-import ReactDOM from "react-dom";
+
 import App from "./App";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe("<App />", () => {
+  it("renders without crashing", () => {
+    shallow(<App />);
+  });
+
+  it("renders children when passed in", () => {
+    const wrapper = shallow((
+      <App>
+        <div className="unique" />
+      </App>
+    ));
+    expect(wrapper.contains(<div className="unique" />)).toEqual(true);
+  });
 });
